68e82644e924dd0e594a2959d5d6094732a7fc7f
jamieberrier <berrier.jamie@gmail.com>
2017-12-04 19:48:53 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 var express = require('express')
 var app = express()
 
+//load controllers
+const SiteController = require("./controllers/SiteController")
 
+//routes
+app.get('/', SiteController.Index)
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
